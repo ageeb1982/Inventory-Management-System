9e01629aab99fd7dd8a893337b57bcd5434b9006
Mazedur Rahman <Mazedur Rahman>
2017-03-06 18:55:09 +0600

    Again re-upload and dyanamic some layouts...

---
+++ InventoryManagement/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Customer Page
+        public ActionResult Customer()
+        {
+            return View();
+        }
+        public ActionResult ManageCustomer()
+        {
+            return View();
+        }
+
+        // Supplier Page
+        public ActionResult Supplier()
+        {
+            return View();
+        }
+
+        // Employee Page
+        public ActionResult Employee()
+        {
+            return View();
+        }
+
+        public ActionResult ManageEmployee()
+        {
+            return View();
+        }
+
+        // Manage Item Page
+        public ActionResult Item()
+        {
+            return View();
+        }
+
+        public ActionResult ManageItem()
+        {
+            return View();
+        }
+
+        public ActionResult ManageItemList()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ InventoryManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InventoryManagement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InventoryManagement")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ed1b01f-897b-4a9c-8127-9d5b73eb6946")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
